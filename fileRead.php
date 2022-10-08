6c31ca8ce21cab6ce64811808597dd5edfa52110
monirpust <mdmonir1997@gmal.com>
2022-10-08 23:54:31 +0600

    file op open read close

---
+++ fileRead.php
@@ -0,0 +1,22 @@
+<?php
+
+$handle = fopen("test.txt", 'r') or die("file not found");
+
+//echo gettype($handle);
+
+if($handle)
+{
+    echo "<br>" . fgets($handle) . "<br>";
+}
+
+echo "Now read rest of file: <br>";
+while(!feof($handle))
+{
+    $data = fgets($handle);
+    echo $data . "<br>";
+}
+
+//closing the file
+echo fclose($handle);
+
+?>
