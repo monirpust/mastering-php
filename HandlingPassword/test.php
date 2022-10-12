ded4d8b68aa32333d08cbf9829e4d9d7f64ea777
monirpust <mdmonir1997@gmal.com>
2022-10-12 23:51:33 +0600

    simple password matching

---
+++ HandlingPassword/test.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hello User</title>
+</head>
+<body>
+    
+    <?php
+    if($_POST["pass"]=="Hello"){?>
+    <h1>Welcome to webapp</h1>
+    <?php } else{ ?>
+
+        <h2> Please enter correct password</h2>
+    <?php } ?>
+
+</body>
+</html>
